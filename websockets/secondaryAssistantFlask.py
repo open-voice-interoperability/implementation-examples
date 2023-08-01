7f158edef680c4d1532d6125031592483d4f375a
Deborah Dahl <dahl@conversational-technologies.com>
2023-08-01 10:12:28 -0400

    Added code for a server to work with the Flask framework

---
+++ websockets/secondaryAssistantFlask.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import request
+from secondaryAssistant import *
+
+app = Flask(__name__)
+secondaryAssistant = SecondaryAssistant()
+@app.route('/', methods=['GET', 'POST'])
+
+def home():
+    data = json.loads(request.data)
+    print(data)
+    response_data = secondaryAssistant.invoke_assistant(data)
+    return response_data
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",port=8766, debug=True)
